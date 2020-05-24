f3fdf2321708b04ec1a84f99e80852151a78242d
Egor Tensin <Egor.Tensin@gmail.com>
2020-05-24 00:08:54 +0000

    makefile_escaping: fix inclusions in other variables

--- makefile_escaping/env_vars.mk
+++ makefile_escaping/env_vars.mk
@@ -1,15 +1,22 @@
 escape = $(subst ','\'',$(1))
-escape_var = $(call escape,$(value $(1)))
 
 test_var ?= This is safe.
+test_var := $(value test_var)
 export test_var
 
-echo_test_var := printf '%s\n' '$(call escape_var,test_var)'
-bash_test_var := bash -c '$(call escape_var,echo_test_var)'
+inner_var := Inner variable
+outer_var := Outer variable - $(inner_var) - $(test_var)
+
+echo_test_var := printf '%s\n' '$(call escape,$(test_var))'
+bash_test_var := bash -c '$(call escape,$(echo_test_var))'
+
+echo_outer_var := printf '%s\n' '$(call escape,$(outer_var))'
 
 .PHONY: test
 test:
-	@printf '%s\n' '$(call escape_var,test_var)'
+	@printf '%s\n' '$(call escape,$(test_var))'
 	@printf '%s\n' "$$test_var"
-	@bash -c '$(call escape_var,echo_test_var)'
-	@bash -c '$(call escape_var,bash_test_var)'
+	@bash -c '$(call escape,$(echo_test_var))'
+	@bash -c '$(call escape,$(bash_test_var))'
+	@printf '%s\n' '$(call escape,$(outer_var))'
+	@bash -c '$(call escape,$(echo_outer_var))'
--- makefile_escaping/shell.mk
+++ makefile_escaping/shell.mk
@@ -3,8 +3,13 @@ escape = $(subst ','\'',$(1))
 cwd := $(shell basename -- "$$( pwd )")
 export cwd
 
+inner_var := Inner variable
+outer_var := Outer variable - $(inner_var) - $(cwd)
+
 echo_cwd := printf '%s\n' '$(call escape,$(cwd))'
 bash_cwd := bash -c '$(call escape,$(echo_cwd))'
+
+echo_outer_var := printf '%s\n' '$(call escape,$(outer_var))'
 
 .PHONY: test
 test:
@@ -12,3 +17,5 @@ test:
 	@printf '%s\n' "$$cwd"
 	@bash -c '$(call escape,$(echo_cwd))'
 	@bash -c '$(call escape,$(bash_cwd))'
+	@printf '%s\n' '$(call escape,$(outer_var))'
+	@bash -c '$(call escape,$(echo_outer_var))'
