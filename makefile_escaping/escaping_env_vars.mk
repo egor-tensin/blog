90a298276f7f9b9c93e8c94ac27c9ed43da8c9e3
Egor Tensin <Egor.Tensin@gmail.com>
2020-05-29 23:26:31 +0000

    makefile_escaping: add comments to *.mk files

--- makefile_escaping/escaping_env_vars.mk
+++ makefile_escaping/escaping_env_vars.mk
@@ -11,12 +11,17 @@ test_var ?= This is safe.
 test_var := $(value test_var)
 export test_var
 
-inner_var := Inner variable
-outer_var := Outer variable - $(inner_var) - $(test_var)
-
+# printf $test_var
 echo_test_var := printf '%s\n' '$(call escape,$(test_var))'
+# bash -c 'printf $test_var'
 bash_test_var := bash -c '$(call escape,$(echo_test_var))'
 
+# Simple variable.
+inner_var := Inner variable
+# Composite variable, includes both $inner_var and $test_var.
+outer_var := Outer variable - $(inner_var) - $(test_var)
+
+# printf $outer_var
 echo_outer_var := printf '%s\n' '$(call escape,$(outer_var))'
 
 .PHONY: test
--- makefile_escaping/escaping_shell.mk
+++ makefile_escaping/escaping_shell.mk
@@ -10,12 +10,17 @@ escape = $(subst ','\'',$(1))
 cwd := $(shell basename -- "$$( pwd )")
 export cwd
 
-inner_var := Inner variable
-outer_var := Outer variable - $(inner_var) - $(cwd)
-
+# printf $cwd
 echo_cwd := printf '%s\n' '$(call escape,$(cwd))'
+# bash -c 'printf $cwd'
 bash_cwd := bash -c '$(call escape,$(echo_cwd))'
 
+# Simple variable.
+inner_var := Inner variable
+# Composite variable, includes both $inner_var and $cwd.
+outer_var := Outer variable - $(inner_var) - $(cwd)
+
+# printf $outer_var
 echo_outer_var := printf '%s\n' '$(call escape,$(outer_var))'
 
 .PHONY: test
