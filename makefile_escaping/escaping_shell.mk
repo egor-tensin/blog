ddeadb135e846cd3fb1f374160b944fd28ce729e
Egor Tensin <Egor.Tensin@gmail.com>
2020-05-29 23:02:49 +0000

    makefile_escaping: add prologue to all *.mk files

--- makefile_escaping/escaping_shell.mk
+++ makefile_escaping/escaping_shell.mk
@@ -1,3 +1,10 @@
+MAKEFLAGS += --warn-undefined-variables
+.DEFAULT_GOAL := all
+.DELETE_ON_ERROR:
+.SUFFIXES:
+SHELL := bash
+.SHELLFLAGS := -e -o pipefail -c
+
 escape = $(subst ','\'',$(1))
 
 cwd := $(shell basename -- "$$( pwd )")
--- makefile_escaping/prologue.mk
+++
@@ -1,6 +0,0 @@
-MAKEFLAGS += --warn-undefined-variables
-.DEFAULT_GOAL := all
-.DELETE_ON_ERROR:
-.SUFFIXES:
-SHELL := bash
-.SHELLFLAGS := -e -o pipefail -c
